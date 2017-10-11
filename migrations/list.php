cc1b90a529fcca107bb67d1bce628d5e3eb48b61
Jess <jessisawesome@hotmail.co.nz>
2017-10-11 14:01:44 +1300

    Initial Commit

---
+++ migrations/list.php
@@ -0,0 +1,8 @@
+<?php
+$migrationList = [];
+$migrationList[] = 'v20171001_200201_update_attendance_out';
+$migrationList[] = 'v20170918_200000_add_attendance_image_out';
+$migrationList[] = 'v20170908_200000_payroll_group';
+$migrationList[] = 'v20170702_190500_add_attendance_image';
+$migrationList[] = 'v20170310_190401_add_timesheet_changes';
+$migrationList[] = 'v20161116_190001_unique_index_cron_name';
