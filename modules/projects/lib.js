4a17b6c276370dd27e6d372e31c585d73386d84a
Jess <jessisawesome@hotmail.co.nz>
2017-10-12 15:27:24 +1300

    Modify employee user UI to show client data, add client adapter data to employee javascript library

--- modules/projects/lib.js
+++ modules/projects/lib.js
@@ -20,6 +20,74 @@ Original work Copyright (c) 2012 [Gamonoid Media Pvt. Ltd]
 Developer: Thilina Hasantha (thilina.hasantha[at]gmail.com / facebook.com/thilinah)
  */
 
+//121017 JEP Copy client adapter information from admin library
+
+ /**
+ * ClientAdapter
+ */
+
+function ClientAdapter(endPoint,tab,filter,orderBy) {
+	this.initAdapter(endPoint,tab,filter,orderBy);
+}
+
+ClientAdapter.inherits(AdapterBase);
+
+
+
+ClientAdapter.method('getDataMapping', function() {
+	return [
+	        "id",
+	        "name",
+	        "details",
+	        "address",
+	        "contact_number"
+	];
+});
+
+ClientAdapter.method('getHeaders', function() {
+	return [
+			{ "sTitle": "ID","bVisible":false },
+			{ "sTitle": "Name" },
+			{ "sTitle": "Details"},
+			{ "sTitle": "Address"},
+			{ "sTitle": "Contact Number"}
+	];
+});
+
+ClientAdapter.method('getFormFields', function() {
+	if(this.showSave){
+		return [
+		        [ "id", {"label":"ID","type":"hidden"}],
+		        [ "name", {"label":"Name","type":"text"}],
+		        [ "details",  {"label":"Details","type":"textarea","validation":"none"}],
+		        [ "address",  {"label":"Address","type":"textarea","validation":"none"}],
+		        [ "contact_number", {"label":"Contact Number","type":"text","validation":"none"}],
+		        [ "contact_email", {"label":"Contact Email","type":"text","validation":"none"}],
+		        [ "company_url", {"label":"Company Url","type":"text","validation":"none"}],
+		        [ "status", {"label":"Status","type":"select","source":[["Active","Active"],["Inactive","Inactive"]]}],
+		        [ "first_contact_date", {"label":"First Contact Date","type":"date","validation":"none"}]
+		];
+	}else{
+		return [
+		        [ "id", {"label":"ID","type":"hidden"}],
+		        [ "name", {"label":"Name","type":"placeholder"}],
+		        [ "details",  {"label":"Details","type":"placeholder","validation":"none"}],
+		        [ "address",  {"label":"Address","type":"placeholder","validation":"none"}],
+		        [ "contact_number", {"label":"Contact Number","type":"placeholder","validation":"none"}],
+		        [ "contact_email", {"label":"Contact Email","type":"placeholder","validation":"none"}],
+		        [ "company_url", {"label":"Company Url","type":"placeholder","validation":"none"}],
+		        [ "status", {"label":"Status","type":"placeholder","source":[["Active","Active"],["Inactive","Inactive"]]}],
+		        [ "first_contact_date", {"label":"First Contact Date","type":"placeholder","validation":"none"}]
+		];
+	}
+});
+
+ClientAdapter.method('getHelpLink', function () {
+	return 'http://blog.icehrm.com/docs/projects/';
+});
+
+ //121017 JEP removing because not actually necessary
+
 function EmployeeProjectAdapter(endPoint) {
 	this.initAdapter(endPoint);
 }
