94f9445992e752a9e82a4e5478399289a7411b90
laznz1 <laznz1@hotmail.com>
2017-10-13 11:15:50 +1300

    Salary Removal
    Removing the Salary .json and change the folder titles. Looking to
    remove Salary.

--- app/config.php
+++ app/config.php
@@ -6,14 +6,14 @@ define('FB_URL', 'Ice Hrm');
 define('TWITTER_URL', 'Ice Hrm');
 
 define('CLIENT_NAME', 'app');
-define('APP_BASE_PATH', 'C:\MAMP\htdocs\servicedeskhrm/');
-define('CLIENT_BASE_PATH', 'C:\MAMP\htdocs\servicedeskhrm\app/');
+define('APP_BASE_PATH', 'C:\xampp\htdocs\ServiceDeskHRM/');
+define('CLIENT_BASE_PATH', 'C:\xampp\htdocs\ServiceDeskHRM\app/');
 define('BASE_URL','http://localhost/servicedeskhrm/');
 define('CLIENT_BASE_URL','http://localhost/servicedeskhrm/app/');
 
 define('APP_DB', 'icehrmsandbox');
-define('APP_USERNAME', 'icehrmuser');
-define('APP_PASSWORD', 'icehrmuser');
+define('APP_USERNAME', 'icehrm_user');
+define('APP_PASSWORD', 'icehrm_user');
 define('APP_HOST', 'localhost');
 define('APP_CON_STR', 'mysqli://'.APP_USERNAME.':'.APP_PASSWORD.'@'.APP_HOST.'/'.APP_DB);
 
