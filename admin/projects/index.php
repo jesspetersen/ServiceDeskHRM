25090c0268fd00fd37994c27aa0e6e20f922fe58
Jess <jessisawesome@hotmail.co.nz>
2017-11-10 01:54:09 +1300

    Remove admin project access - no longer needed. Add filtering for employees and clients on admin screen.

--- admin/projects/index.php
+++ admin/projects/index.php
@@ -32,7 +32,6 @@ $projectClient->addEmployeeClientRows();
 			  
 	<ul class="nav nav-tabs" id="modTab" style="margin-bottom:0px;margin-left:5px;border-bottom: none;">
 		<li class="active"><a id="tabClient" href="#tabPageClient"><?=t('Clients')?></a></li>
-		<li><a id="tabProject" href="#tabPageProject"><?=t('Projects')?></a></li>
 		<li><a id="tabEmployeeProject" href="#tabPageEmployeeProject"><?=t('Employee Projects')?></a></li>
 	</ul>
 	 
@@ -42,14 +41,6 @@ $projectClient->addEmployeeClientRows();
 		
 			</div>
 			<div id="ClientForm" class="reviewBlock" data-content="Form" style="padding-left:5px;display:none;">
-		
-			</div>
-		</div>
-		<div class="tab-pane" id="tabPageProject">
-			<div id="Project" class="reviewBlock" data-content="List" style="padding-left:5px;">
-		
-			</div>
-			<div id="ProjectForm" class="reviewBlock" data-content="Form" style="padding-left:5px;display:none;">
 		
 			</div>
 		</div>
@@ -73,38 +64,17 @@ modJsList['tabClient'] = new ClientAdapter('Client','Client');
 modJsList['tabClient'].setShowAddNew(false);
 <?php }?>
 
-<?php if(isset($modulePermissions['perm']['Delete Clients']) && $modulePermissions['perm']['Delete Clients'] == "No"){?>
 modJsList['tabClient'].setShowDelete(false);
-<?php }?>
 
 <?php if(isset($modulePermissions['perm']['Edit Clients']) && $modulePermissions['perm']['Edit Clients'] == "No"){?>
 modJsList['tabClient'].setShowSave(false);
 <?php }?>
 
-modJsList['tabProject'] = new ProjectAdapter('Project','Project');
-
-<?php if(isset($modulePermissions['perm']['Add Projects']) && $modulePermissions['perm']['Add Projects'] == "No"){?>
-modJsList['tabProject'].setShowAddNew(false);
-<?php }?>
-
-<?php if(isset($modulePermissions['perm']['Delete Projects']) && $modulePermissions['perm']['Delete Projects'] == "No"){?>
-modJsList['tabProject'].setShowDelete(false);
-<?php }?>
-
-<?php if(isset($modulePermissions['perm']['Edit Projects']) && $modulePermissions['perm']['Edit Projects'] == "No"){?>
-modJsList['tabProject'].setShowSave(false);
-<?php }?>
-
-
 modJsList['tabEmployeeProject'] = new EmployeeProjectAdapter('EmployeeProject','EmployeeProject');
 modJsList['tabEmployeeProject'].setRemoteTable(true);
 
-<?php if(isset($modulePermissions['perm']['Add Projects']) && $modulePermissions['perm']['Add Projects'] == "No"){?>
 modJsList['tabEmployeeProject'].setShowAddNew(false);
-<?php }?>
-<?php if(isset($modulePermissions['perm']['Delete Projects']) && $modulePermissions['perm']['Delete Projects'] == "No"){?>
 modJsList['tabEmployeeProject'].setShowDelete(false);
-<?php }?>
 <?php if(isset($modulePermissions['perm']['Edit Projects']) && $modulePermissions['perm']['Edit Projects'] == "No"){?>
 modJsList['tabEmployeeProject'].setShowEdit(false);
 <?php }?>
--- admin/projects/lib.js
+++ admin/projects/lib.js
@@ -66,68 +66,6 @@ ClientAdapter.method('getHelpLink', function () {
 	return 'http://blog.icehrm.com/docs/projects/';
 });
 
-/**
- * ProjectAdapter
- */
-
-function ProjectAdapter(endPoint,tab,filter,orderBy) {
-	this.initAdapter(endPoint,tab,filter,orderBy);
-}
-
-ProjectAdapter.inherits(AdapterBase);
-
-
-
-ProjectAdapter.method('getDataMapping', function() {
-	return [
-	        "id",
-	        "name",
-		"client",
-		"account",
-		"training"
-	];
-});
-
-ProjectAdapter.method('getHeaders', function() {
-	return [
-			{ "sTitle": "ID","bVisible":false },
-			{ "sTitle": "Name" },
-			{ "sTitle": "Client"},
-			{ "sTitle": "Has Account"},
-			{ "sTitle": "Has Training"}
-	];
-});
-
-ProjectAdapter.method('getFormFields', function() {
-
-	if(this.showSave){
-		return [
-		        [ "id", {"label":"ID","type":"hidden"}],
-		        [ "name", {"label":"Name","type":"text"}],
-		        [ "client", {"label":"Client","type":"select2","allow-null":true,"remote-source":["Client","id","name"]}],
-			[ "details",  {"label":"Details","type":"textarea","validation":"none"}],
-			[ "account", {"label":"Has Account","type":"select","source":[["true","True"],["false","False"]]}],
-			[ "training", {"label":"Has Training","type":"select","source":[["true","True"],["false","False"]]}],
-		        [ "status", {"label":"Status","type":"select","source":[["Active","Active"],["On Hold","On Hold"],["Completed","Completed"],["Dropped","Dropped"]]}]
-		];
-	}else{
-		return [
-		        [ "id", {"label":"ID","type":"hidden"}],
-		        [ "name", {"label":"Name","type":"placeholder"}],
-		        [ "client", {"label":"Client","type":"placeholder","allow-null":true,"remote-source":["Client","id","name"]}],
-			[ "details",  {"label":"Details","type":"placeholder","validation":"none"}],
-			[ "account", {"label":"Has Account","type":"select","source":[["true","True"],["false","False"]]}],
-			[ "training", {"label":"Has Training","type":"select","source":[["true","True"],["false","False"]]}],
-			[ "status", {"label":"Status","type":"select","source":[["Active","Active"],["On Hold","On Hold"],["Completed","Completed"],["Dropped","Dropped"]]}]
-		];
-	}
-	
-});
-
-ProjectAdapter.method('getHelpLink', function () {
-	return 'http://blog.icehrm.com/docs/projects/';
-});
-
 
 /*
  * EmployeeProjectAdapter
@@ -175,8 +113,8 @@ EmployeeProjectAdapter.method('getFormFields', function() {
 
 EmployeeProjectAdapter.method('getFilters', function() {
 	return [
-	        [ "employee", {"label":"Employee","type":"select2","remote-source":["Employee","id","first_name+last_name"]}]
-	        
+		[ "employee", {"label":"Employee","type":"select2","allow-null":true,"null-label":"All Employees","remote-source":["Employee","id","first_name+last_name"],"validation":"none"}],
+		[ "client", {"label":"Client","type":"select2","allow-null":true,"null-label":"All Clients","remote-source":["Client","id","name"],"validation":"none"}]
 	];
 });
 
